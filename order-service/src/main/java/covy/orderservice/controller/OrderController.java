04f315635d01b7de165b0f68a2f93529a6a6bad8
covy <junni802@naver.com>
2025-04-18 17:39:11 +0900

    feat: add dockerfile

--- order-service/src/main/java/covy/orderservice/controller/OrderController.java
+++ order-service/src/main/java/covy/orderservice/controller/OrderController.java
@@ -3,9 +3,13 @@ package covy.orderservice.controller;
 import com.netflix.discovery.converters.Auto;
 import covy.orderservice.dto.OrderDto;
 import covy.orderservice.entity.OrderEntity;
+import covy.orderservice.messagequeue.producer.KafkaProducer;
+import covy.orderservice.messagequeue.producer.OrderProducer;
 import covy.orderservice.service.OrderService;
 import covy.orderservice.vo.RequestOrder;
 import covy.orderservice.vo.ResponseOrder;
+import java.util.UUID;
+import lombok.extern.slf4j.Slf4j;
 import org.modelmapper.ModelMapper;
 import org.modelmapper.convention.MatchingStrategies;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -18,16 +22,21 @@ import java.util.ArrayList;
 import java.util.List;
 
 @RestController
+@Slf4j
 @RequestMapping("/order-service")
 public class OrderController {
 
     Environment env;
     OrderService orderService;
+    KafkaProducer kafkaProducer;
+    OrderProducer orderProducer;
 
     @Autowired
-    public OrderController(Environment env, OrderService orderService) {
+    public OrderController(Environment env, OrderService orderService, KafkaProducer kafkaProducer, OrderProducer orderProducer) {
         this.env = env;
         this.orderService = orderService;
+        this.kafkaProducer = kafkaProducer;
+        this.orderProducer = orderProducer;
     }
 
 
@@ -42,22 +51,33 @@ public class OrderController {
         mapper.getConfiguration().setMatchingStrategy(MatchingStrategies.STRICT);
         OrderDto orderDto = mapper.map(order, OrderDto.class);
         orderDto.setUserId(userId);
-        orderService.createOrder(orderDto);
+        /* jpa */
+//        orderService.createOrder(orderDto);
+//        ResponseOrder responseOrder = mapper.map(orderDto, ResponseOrder.class);
+
+        /* kafka */
+        orderDto.setOrderId(UUID.randomUUID().toString());
+        orderDto.setTotalPrice(order.getQty() * order.getUnitPrice());
+
+        /* send this order */
+        kafkaProducer.send("example-catalog-topic", orderDto);
+        orderProducer.send("orders", orderDto);
 
         ResponseOrder responseOrder = mapper.map(orderDto, ResponseOrder.class);
-
 
         return ResponseEntity.status(HttpStatus.CREATED).body(responseOrder);
     }
 
     @GetMapping("/{userId}/orders")
     public ResponseEntity<List<ResponseOrder>> getUser(@PathVariable("userId") String userId) {
+        log.info("Before call orders microservice");
         Iterable<OrderEntity> order = orderService.getOrdersByUserId(userId);
 
         List<ResponseOrder> result = new ArrayList<>();
         order.forEach(v -> {
             result.add(new ModelMapper().map(v, ResponseOrder.class));
         });
+        log.info("After call orders microservice");
 
         return ResponseEntity.status(HttpStatus.OK).body(result);
     }

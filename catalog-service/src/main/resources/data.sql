a135ec7e226fdc6c2a623d571ef46a55bf91e7c0
covy <junni802@naver.com>
2025-04-22 18:44:52 +0900

    feat: add

--- catalog-service/src/main/resources/data.sql
+++ catalog-service/src/main/resources/data.sql
@@ -1,2 +1,6 @@
 INSERT INTO catalog(product_id, product_name, stock, unit_price)
-    VALUES ()
+    VALUES ('CATALOG-001', 'Berlin', 100, 1500);
+INSERT INTO catalog(product_id, product_name, stock, unit_price)
+    VALUES ('CATALOG-002', 'Tokyo', 110, 1000);
+INSERT INTO catalog(product_id, product_name, stock, unit_price)
+    VALUES ('CATALOG-003', 'Stockhole', 120, 2200);
